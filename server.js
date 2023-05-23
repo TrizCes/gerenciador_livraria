e54bf201cfb1ff7ab41f201abb0487a5b71a14c5
Beatriz Ceschini <biaceschini@gmail.com>
2023-05-22 22:36:25 -0300

    Parte 1

---
+++ server.js
@@ -0,0 +1,18 @@
+const http = require('http');
+const port = 3000;
+
+const rotas = {
+  '/': 'Curso de Node',
+  '/livros': 'Pagina de livros',
+  '/autores': 'Listagem de autores',
+  '/sobre': 'Informações do projeto',
+};
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end(rotas[req.url]);
+});
+
+server.listen(port, () => {
+  console.log(`Servidor escutando na porta http://localhost:${port}`);
+});
