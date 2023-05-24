b16ad064304faaff07e7c36ff3a4b40d5d3ca3eb
Beatriz Ceschini <biaceschini@gmail.com>
2023-05-24 15:49:32 -0300

    relacionando coleções

--- src/controllers/livrosController.js
+++ src/controllers/livrosController.js
@@ -2,7 +2,9 @@ import livros from '../models/Livro.js';
 
 class LivroController {
   static listarLivros = (req, res) => {
-    livros.find((err, livros) => {
+    livros.find()
+    .populate('autor')
+    .exec((err, livros) => {
       res.status(200).json(livros);
     });
   };
@@ -10,7 +12,9 @@ class LivroController {
   static listarLivroPorId = (req, res) => {
     const id = req.params.id;
 
-    livros.findById(id, (err, livros) => {
+    livros.findById(id)
+    .populate('autor', 'nome')
+    .exec((err, livros) => {
       if (err) {
         res.status(400).send({ message: `${err.message} - Id do livro não localizado.` });
       } else {
@@ -54,6 +58,13 @@ class LivroController {
       }
     });
   };
+
+  static listarLivroPelaEditora = (req, res) => {
+    const editora = req.query.editora;
+    livros.find({ 'editora': editora }, {}, (err, livros) =>{
+      res.status(200).send(livros);
+    })
+  }
 }
 
 export default LivroController;
